cdfff799084ff222975a13dbdcfd1470de12c656
James Kozianski <james@ubuntu.(none)>
2008-07-12 23:12:18 +1000

    WIP.

--- Main.hs
+++ Main.hs
@@ -7,58 +7,105 @@ import MessageParser
 import System.IO
 import Prelude hiding(Left, Right)
 import Control.Concurrent.MVar
+import Control.Concurrent
 import System.IO.Unsafe
+import Network.Socket
 
 targetPoint :: MVar Point
 targetPoint = unsafePerformIO (newMVar (0,0))
 
-main' :: Memory -> IO ()
-main' memory = do
+main' :: MVar Memory -> (String -> IO ()) -> IO ()
+main' memoryMV puts = do
+  mem <- readMVar memoryMV
+
+  -- Write the targetPoint MVar.
+  calculateTargetPoint mem targetPoint
+
+  tp <- readMVar targetPoint
+
+  -- Send the command to the server.
+  sendMessage (getCommand mem tp) puts
+
+  let cmd = getCommand mem tp
+
+
+  main' memoryMV puts
+
+-- Starts the various threads in the system.
+startThreads :: IO ()
+startThreads = do
+  memoryMV <- newEmptyMVar
+  (networkGetChar, networkPutString) <- f
+  forkIO (updateMemoryThread memoryMV networkGetChar)
+  main' memoryMV networkPutString
+
+main = do
   hSetBuffering stdout NoBuffering
-  msg <- getMessage
+  startThreads
+
+forever m = m >> forever m
+
+-- The thread that listens for network input and updates the memory.
+updateMemoryThread :: MVar Memory -> IO Char -> IO ()
+updateMemoryThread memoryMV getc = forever $ do
+  msg <- getMessage getc
   case msg of
-    (Init initMessage) -> main' (memory{memTrialInfo = initMessage})
-    (Telem telemData)  -> continue (updateTelem telemData memory)
-    _                  -> continue memory
+    (Init initMessage) -> createOrInitializeMemory memoryMV initMessage
+    (Telem telemData)  -> liftMemUpdate $ updateTelem telemData
+    _                  -> return ()
+
   where
-    continue mem = do
-      calculateTargetPoint mem targetPoint
-      tp <- readMVar targetPoint
-      sendMessage (getCommand mem tp)
-      main' mem
+    createOrInitializeMemory mv init = do
+      maybeMem <- tryTakeMVar mv
 
-performTrial :: IO ()
-performTrial = do
-  (Init initMessage) <- getMessage
-  memoryMV <- newMVar (mkMemory initMessage)
-  main' (mkMemory initMessage)
+      case maybeMem of
+        Just mem -> putMVar mv (mem{ memTrialInfo = init })
+        Nothing  -> putMVar mv (mkMemory init)
 
-main = forever performTrial
+    liftMemUpdate t = do
+      putStrLn "Received a message."
+      m <- takeMVar memoryMV
+      putMVar memoryMV (t m)
 
-forever p = p >> forever p
-  
-getMessage :: IO Message
-getMessage = getMessage' ""
+getMessage :: IO Char -> IO Message
+getMessage getc = getMessage' getc ""
 
-getMessage' :: String -> IO Message
-getMessage' str = do
-  c <- getChar
+getMessage' :: IO Char -> String -> IO Message
+getMessage' getc str = do
+  c <- getc
   if (c == ';') then return (parseMessage (reverse str))
-                else getMessage' (c:str)
+                else getMessage' getc (c:str)
 
+e c = hPutStr stderr [c,';']
 -- |Send a message to the serve
-sendMessage :: VehicleControl -> IO ()
-sendMessage vc = do
+sendMessage :: VehicleControl -> (String -> IO ()) -> IO ()
+sendMessage vc puts = do
                   sendAcc $ vcAcc vc
                   sendDir $ vcDir vc
-                  putStr ";"
+                  lPuts "o;"
   where
-    sendAcc Accelerate = putStr "a"
-    sendAcc Brake      = putStr "b"
+    sendAcc Accelerate = lPuts "a"
+    sendAcc Brake      = lPuts "b"
     sendAcc Roll       = return ()
 
-    sendDir HardLeft  = putStr "l;l"
-    sendDir Left      = putStr "l"
+    sendDir HardLeft  = lPuts "l;l"
+    sendDir Left      = lPuts "l"
     sendDir Straight  = return ()
-    sendDir Right     = putStr "r"
-    sendDir HardRight = putStr "r;r"
+    sendDir Right     = lPuts "r"
+    sendDir HardRight = lPuts "r;r"
+
+
+    lPuts str = do
+      hPutStrLn stderr ("sending: " ++ str)
+      puts str
+
+
+f :: IO Handle
+f = do
+  sock <- socket AF_INET Stream defaultProtocol
+  hostAddr <- inet_addr "127.0.0.1"
+  connect sock (SockAddrInet 17676 hostAddr)
+  return (networkGetChar sock, networkPutStr sock)
+  where
+    networkGetChar sock = (recv sock 1) >>= return . head
+    networkPutStr sock str = send sock str >> return ()
